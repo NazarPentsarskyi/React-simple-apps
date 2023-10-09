d6daa3f8cc090981e2b25eb8d3b1f9a598174b45
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-10-09 23:00:08 +0300

    added timer app

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,61 @@
 import { Link } from "react-router-dom";
 
+import { useEffect, useState } from "react";
+
 const Timer = () => {
+
+const [time, setTime] = useState(0);
+const [isRunning, setIsRunning] = useState(false);
+
+//const startTime = Math.floor(Date.now() / 1000);
+
+const handleStart = () => {
+  setTime(0);
+  setIsRunning(true);
+  
+  if(isRunning === false) {
+    setTime(time);
+  }
+}
+
+const handleStop = () => {
+  setIsRunning(false);
+}
+
+const handleReset = () => {
+  setTime(0);
+  setIsRunning(false);
+}
+
+useEffect (() => {
+  
+  if ( isRunning === true) {
+  
+    const timeoutId = setTimeout(() => {
+      setTime(prevTime => prevTime + 1);
+  }, 1000)
+
+  return () => {
+    if(isRunning === false) {
+      clearTimeout(timeoutId);
+      setIsRunning(false);
+    }
+  }
+}
+},[time, isRunning])
+
   return (
     <>
       <button><Link to="/"> Home </Link></button>
       <h2>Timer</h2>
+      <br />
+
+      <h3>{time}</h3>
+      <div className='board'>
+        <button onClick = {handleStart}>Start</button>
+        <button onClick = {handleStop}>Stop</button>
+        <button onClick = {handleReset}>Reset</button>
+      </div>
     </> 
   )
 }
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,7 +5,6 @@ export const Navbar = () => {
   return (
     <div>
       <h1>React simple apps</h1>
-        <div className='link'><Link to="/"> Home </Link></div>
         <div className='link'><Link to="/counter"> 01. Counter</Link></div>
         <div className='link'><Link to="/calculator"> 02. Calculator</Link></div>
         <div className='link'><Link to="/timer"> 03. Timer</Link></div>
