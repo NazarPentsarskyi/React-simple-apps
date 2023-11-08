fec4907066b46f72c0855cfe9df8e3df3072cb12
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-11-09 01:40:40 +0200

    added positive-negative button in calculator app

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -47,6 +47,12 @@ const Calculator = () => {
     }
   };
 
+  const handlePositiveNegative = () => {
+    if (operand !== '') {
+      setOperand((prevOperand) => -prevOperand);
+    }
+  };
+
   const handleCalculate = () => {
     if (operand !== '' && operator !== '') {
       let calc = eval(result + operator + operand);
@@ -77,23 +83,24 @@ const Calculator = () => {
         </label>
         </form>
       <div className="boardCalculator">
+        <button className="highlight" onClick={handleReset}>C</button>
+        <button className="highlight" onClick={handleBack}>{`<x`}</button>
+        <button name = ' ' className="highlight" onClick={handlePositiveNegative}>+/-</button>
+        <button name = '/' className="highlight" onClick={handleOperator}>/</button>
         <button name = '7' onClick={handleOperand}>7</button>
         <button name = '8' onClick={handleOperand}>8</button>
         <button name = '9' onClick={handleOperand}>9</button>
-        <button name = '/' onClick={handleOperator} className="highlight">/</button>
+        <button name = '*' className="highlight" onClick={handleOperator}>x</button>
         <button name = '4' onClick={handleOperand}>4</button>
         <button name = '5' onClick={handleOperand}>5</button>
         <button name = '6' onClick={handleOperand}>6</button>
-        <button name = '*' onClick={handleOperator} className="highlight">*</button>
+        <button name = '-' className="highlight" onClick={handleOperator}>-</button>
         <button name = '1' onClick={handleOperand}>1</button>
         <button name = '2' onClick={handleOperand}>2</button>
         <button name = '3' onClick={handleOperand}>3</button>
-        <button name = '-' onClick={handleOperator}className="highlight">-</button>
-        <button className="highlight" onClick={handleBack}>C</button>
+        <button name = '+' className="highlight" onClick={handleOperator}>+</button>
         <button name = '0' onClick={handleOperand}>0</button>
-        <button name = '.' onClick={handleOperand}className="highlight">.</button>
-        <button name = '+' onClick={handleOperator}className="highlight">+</button>
-        <button className="highlight" onClick={handleReset}>Clear</button>
+        <button name = '.' className="highlight" onClick={handleOperand}>.</button>
         <button className="equal highlight" onClick={handleCalculate}>=</button>
       </div>
     </> 
